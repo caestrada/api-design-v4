6ff9ecde48e7b3f072a3649f128eaf22c51142aa
Carlos Estrada <cestr008@gmail.com>
2023-04-14 14:41:10 -0400

    Setup routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import router from './router';
+
 const app = express();
 
 app.get('/', (req, res) => {
     console.log('Hello, world!');
     res.status(200).json({ message: 'Hello, world!!!!' });
 });
+
+app.use('/api', router);
 
 export default app;
