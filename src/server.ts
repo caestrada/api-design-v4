d102d15fc1a299f46f918595697fd3a82702fb6e
Carlos Estrada <cestr008@gmail.com>
2023-04-14 15:10:17 -0400

    Add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
+// Middlewares
+app.use(cors()); // Allows cross-origin requests.
+app.use(morgan('dev'));
+app.use(express.json()); // For parsing application/json. Allows clients to send JSON data to the server.
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded. Allows clients to send data to the server in a URL-encoded format.
+app.use((req, res, next) => {
+    req.shhh_secret = 'doggy';
+    next();
+})
 app.get('/', (req, res) => {
     console.log('Hello, world!');
     res.status(200).json({ message: 'Hello, world!!!!' });
