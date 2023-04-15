e6bbc971fef150e06592a8b44f9ca8c211d16644
Carlos Estrada <cestr008@gmail.com>
2023-04-15 13:10:07 -0400

    Add error handler

--- src/server.ts
+++ src/server.ts
@@ -13,16 +13,21 @@ app.use(morgan('dev'));
 app.use(express.json()); // For parsing application/json. Allows clients to send JSON data to the server.
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded. Allows clients to send data to the server in a URL-encoded format.
 app.use((req, res, next) => {
-    req.shhh_secret = 'doggy';
-    next();
-})
+  req.shhh_secret = 'doggy';
+  next();
+});
 app.get('/', (req, res) => {
-    console.log('Hello, world!');
-    res.status(200).json({ message: 'Hello, world!!!!' });
+  console.log('Hello, world!');
+  res.status(200).json({ message: 'Hello, world!!!!' });
 });
 
 app.use('/api', protect, router);
 app.post('/user', createNewUser);
 app.post('/signin', signin);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ message: `had an error: ${err.message}` });
+});
 
 export default app;
