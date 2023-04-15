0f41a599112af1fda8535eedcc2645d0daccbc7a
Carlos Estrada <cestr008@gmail.com>
2023-04-15 13:40:44 -0400

    Setup environments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+import config from './config';
 
 import app from './server';
 
-app.listen(3000, () => {
-    console.log('Starting server...');
+app.listen(config.port, () => {
+  console.log(`Starting server on http://localhost:${config.port}`);
 });
